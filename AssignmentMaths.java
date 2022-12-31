607e82b5d92b135b32364885d748bf8ce8b0e823
Kshanty <119849357+Kshanty@users.noreply.github.com>
2022-12-31 12:18:31 +0530

    Add files via upload

---
+++ AssignmentMaths.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class AssignmentMaths {
+
+	public static void main(String[] args) {
+		int a=2,b=4,c=7,d=5,e=6,f=3;
+		Maths Object= new Maths();
+		System.out.println(a+"+ "+b+"="+Object.NumAdd(a, b));
+System.out.println(c+"- "+d+"= "+Object.NumSub(c, d));
+System.out.println(e+"into "+f+"= "+Object.multiply(e, f));
+
+	}
+
+}
---
+++ Maths.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Maths {
+
+	public double NumAdd(int a, int b)
+	{
+		double sum=a+b;
+		return sum;
+	}
+	public double NumSub(int c, int d) {
+		double diff= c-d;
+		return diff;
+	}
+	public double multiply(int x, int y)
+	{
+		double prod=x*y;
+		return prod;
+	}
+
+}
