896fbd935392accb7ddf08b646e0134b67040cfa
Kshanty <119849357+Kshanty@users.noreply.github.com>
2022-12-31 12:31:10 +0530

    Add files via upload

---
+++ Assign1.java
@@ -0,0 +1,10 @@
+package week2.day2B;
+
+public class Assign1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ TestleafOpen.java
@@ -0,0 +1,49 @@
+package week2.day2B;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestleafOpen {
+
+	public static void main(String[] args) {
+
+		
+		
+
+			WebDriverManager.chromedriver().setup();
+			ChromeDriver driver = new ChromeDriver();
+			driver.manage().window().maximize();
+
+			driver.get("http://leaftaps.com/opentaps/control/main");
+		WebElement username = driver.findElement(By.id("username"));
+		username.sendKeys("Democsr");
+		WebElement password = driver.findElement(By.id("password"));
+		password.sendKeys("crmsfa");
+		WebElement login = driver.findElement(By.className("decorativeSubmit"));
+		login.click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+
+		//Classroom:
+		//Source dropdown -> Select value as "Conference" using visible text
+		//Inductry dropdown -> Select value as "Finance" using value
+		//Ownership dropdown -> Select value as "Corporation" using Index
+		Select dropdown = new Select(driver.findElement(By.id("createLeadForm_dataSourceId")));  
+		dropdown.selectByVisibleText("Conference");
+		Select dropdown1 = new Select(driver.findElement(By.id("createLeadForm_industryEnumId")));  
+		dropdown1.selectByValue("IND_FINANCE");
+		
+		Select dropdown2 = new Select(driver.findElement(By.id("createLeadForm_ownershipEnumId")));  
+		dropdown2.selectByIndex(5);
+		
+		
+		//WebElement eleSource = driver.findElement(By.id(null))
+		
+	}
+
+}
